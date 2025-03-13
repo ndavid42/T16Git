d9f0582931169b93e2630b12e9975938a11ace4a
David Nemeth <38688353+ndavid42@users.noreply.github.com>
2025-03-13 19:35:40 +0100

    Update 20250313_kektura.js
    visszajavítottam a javítást, bele vagyok keveredve már...

--- 20250313_kektura.js
+++ 20250313_kektura.js
@@ -167,12 +167,12 @@ function legmagasabb(pecsetelohelyek) {
 	let aktualis = 192;
 	let legmagasabb = [192, 0]; //legmagasabb pont, legmagasabb szakasz
 	for (let i = 1; i < pecsetelohelyek.length; i++) {
-		aktualis += pecsetelohelyek[i].emelkedes;
+		let abs += pecsetelohelyek[i].emelkedes - pecsetelohelyek[i].lejtes;
+		aktualis += abs;
 		if (aktualis > legmagasabb[0]) {
 			legmagasabb[0] = aktualis;
 			legmagasabb[1] = i;
 		}
-		aktualis -= pecsetelohelyek[i].lejtes;
 	}
 	
 	return legmagasabb;
